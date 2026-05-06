c6b8fcde75681974a208d57606c5cb42e6b3b32c
unknown <dudahpereira13@gmail.com>
2026-05-05 22:21:46 -0300

    aojhcowdj

--- demo/src/main/java/meufoslfnapovfhjau/demo/repositories/PessoaRepository.java
+++ demo/src/main/java/meufoslfnapovfhjau/demo/repositories/PessoaRepository.java
@@ -3,5 +3,5 @@ package meufoslfnapovfhjau.demo.repositories;
 import meufoslfnapovfhjau.demo.models.PessoaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PessoaRepository extends JpaRepository<Long, PessoaModel> {
+public interface PessoaRepository extends JpaRepository<PessoaModel, Long> {
 }
--- demo/src/main/java/meufoslfnapovfhjau/demo/services/PessoaService.java
+++ demo/src/main/java/meufoslfnapovfhjau/demo/services/PessoaService.java
@@ -1,11 +1,13 @@
 package meufoslfnapovfhjau.demo.services;
 
+import jakarta.persistence.Table;
 import meufoslfnapovfhjau.demo.models.PessoaModel;
 import meufoslfnapovfhjau.demo.repositories.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PessoaService {
@@ -13,5 +15,21 @@ public class PessoaService {
     @Autowired
     PessoaRepository pessoaRepository;
 
+
+    public PessoaModel criarPessoa(PessoaModel pessoaModel){
+        return pessoaRepository.save(pessoaModel);
+    }
+
+    public List<PessoaModel> findAll(){
+        return pessoaRepository.findAll();
+    }
+
+    public Optional<PessoaModel> buscarId(Long id){
+        return autorRepository.findById(id);
+    }
+
+    public void deletar(Long id){
+        autorRepository.deleteById(id);
+    }
 
 }
